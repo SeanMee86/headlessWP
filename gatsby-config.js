daa25b046f0dd189dee6e98df9055a41f59c0752
SeanMee86 <sean.mee86@yahoo.com>
2020-07-22 07:41:57 -0700

    removing background image

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,17 +9,17 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    // {
-    //   resolve: "gatsby-plugin-google-tagmanager-delayed",
-    //   options: {
-    //     id: "GTM-TDSKB9Q",
-    //
-    //     // Include GTM in development.
-    //     //
-    //     // Defaults to false meaning GTM will only be loaded in production.
-    //     defaultDataLayer: { platform: "gatsby" }
-    //   },
-    // },
+    {
+      resolve: "gatsby-plugin-google-tagmanager-delayed",
+      options: {
+        id: "GTM-TDSKB9Q",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        defaultDataLayer: { platform: "gatsby" }
+      },
+    },
     {
       resolve: `gatsby-plugin-catch-links`,
       options: {
