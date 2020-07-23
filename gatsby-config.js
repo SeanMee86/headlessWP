5c92e0eecbf36254b69efa985126effd05bc155d
SeanMee86 <sean.mee86@yahoo.com>
2020-07-23 07:11:45 -0700

    testing purged css

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,17 +9,28 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    // {
-    //   resolve: "gatsby-plugin-google-tagmanager-delayed",
-    //   options: {
-    //     id: "GTM-TDSKB9Q",
-    //
-    //     // Include GTM in development.
-    //     //
-    //     // Defaults to false meaning GTM will only be loaded in production.
-    //     defaultDataLayer: { platform: "gatsby" }
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager-delayed",
+      options: {
+        id: "GTM-TDSKB9Q",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        defaultDataLayer: { platform: "gatsby" }
+      },
+    },
     {
       resolve: `gatsby-plugin-catch-links`,
       options: {
