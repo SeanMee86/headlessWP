8bd5b91626ce9c382ef5f4cd53c8c9188e641950
SeanMee86 <sean.mee86@yahoo.com>
2020-07-23 06:38:02 -0700

    updated fonts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,12 +5,5 @@
  */
 
 // You can delete this file if you're not using it
-import React from "react";
 
-export const onRenderBody = ({setHeadComponents}) => {
-    setHeadComponents([
-        <meta name={'referrer'} content={'origin'} />,
-        <script src="https://73f6c8313e3d44a2bdef7d6220d114fd.js.ubembed.com" async/>
-    ])
-}
 
